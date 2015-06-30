e601e1015839769e0aa4c3722e36219f068a5e2f
Gelacio Lazaro <glazaro@artedigital-mx.com>
2015-06-29 22:21:39 -0700

    Initial Commit

---
+++ EarthQuakeMonitorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EarthQuakeMonitorApp
+//
+//  Created by Arte Digital on 6/29/15.
+//  Copyright (c) 2015 Gelacio Lazaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
